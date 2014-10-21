16d38e6ef4f9779aeff2ee5bc5bf1176ed5a83c8
Hristiyan Yordanov <mr.yordanoff@gmail.com>
2014-10-21 13:39:28 +0300

    Range collision handler done

--- TowerDeffence/Helpers/RangeCollisionHandler.cs
+++ TowerDeffence/Helpers/RangeCollisionHandler.cs
@@ -1,6 +1,19 @@
 ﻿namespace TowerDeffence.Helpers
 {
+    using System;
+    using TowerDeffence.GameObjects;
+    using TowerDeffence.Interfaces;
     public class RangeCollisionHandler : CollisionHandler
     {
+        public bool isInRange(ITower Tower, BattleUnit Enemy)
+        {
+            double towerX = Tower.Position.X;
+            double towerY = Tower.Position.Y;
+            double towerRange = Tower.Range;
+            double enemyX = Enemy.Position.X;
+            double enemyY = Enemy.Position.Y;
+
+            return Math.Pow((enemyX - towerX), 2) + Math.Pow((enemyY - towerY), 2) <= Math.Pow(towerRange, 2);
+        }
     }
 }
